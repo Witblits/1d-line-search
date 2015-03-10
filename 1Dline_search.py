1eafe8096786696d32bf9b6ddc05127ab69c4704
Andreas Joss <16450612@sun.ac.za>
2015-03-10 22:55:36 +0200

    All three algorithms implemented. Not sure the purpose or correctness of algorithm 2. Output graphs also added to git.

--- 1Dline_search.py
+++ 1Dline_search.py
@@ -21,6 +21,9 @@ r = 0.618034		#golden ratio
 algorithm_string = ""
 example_string = ""
 graph = []
+graph1 = []
+graph2 = []
+x_initial = 0
 ##########################################################################################
 
 
@@ -98,24 +101,65 @@ L = b - a		#Length of line search is conducted on
 ###OPTIMIZATION METHODS
 ##########################################################################################
 ### 1-D line search based on the Newton-Raphson Method
-def lineSearchNewtonRapshon(a,b,L,r):
+def lineSearchNewtonRapshon(a,b):
   solution = pl.np.zeros((3,1))
   solution_found = False
+  i = 0
+  x_old = (b+a)/2 		#assume a solution nearby (works good for all test functions except Test Function 4 which does not converge)
+  #x_old = 0.9*b			#works for all test functions (just requires more iterations for some)
+  x = x_old
+  global x_initial
+  x_initial = x_old
+  #print "x_old: %.3f" %x_old
+  #print "x    : %.3f" %x
   
-  x = (b+a)/2 		#assume a solution nearby
+  while (pl.np.abs(x-x_old) > epsilon1) or (i == 0):
+    x_old = x
+    x = x_old - (testFunction(1,x_old)/testFunction(2,x_old))
+    i = i + 1
+    #print "pl.np.abs(x-x_old) : %.3f" %pl.np.abs(x-x_old)
+    #print "i	: %d" %i
+    #print "x_old: %.3f" %x_old
+    #print "x    : %.3f" %x
+    #print "f(x) : %.3f" %testFunction(0,x_old)
   
-  #x = x - 
-  
-  
-  
+  solution[0] = x
+  solution[1] = testFunction(0,x)
+  solution[2] = i
+  solution_found = True 
   return solution
 
 ### Modification of the Newton-Raphson we discussed in class
 def modifiedNewtonRapshon(a,b,L,r):
   solution = pl.np.zeros((3,1))
   solution_found = False
+  epsilon1 = 1e-5
+  i = 0
+  #x_old = (b+a)/2 		#assume a solution nearby (works good for all test functions except Test Function 4 which does not converge)
+  x_old = 0.9*b			#works for all test functions (just requires more iterations for some)
+  x = x_old
+  global x_initial
+  x_initial = x_old
+  #print "x_old: %.3f" %x_old
+  #print "x    : %.3f" %x
   
+  while (pl.np.abs(x-x_old) > epsilon1) or (i == 0):
+    x_old = x
+    numerator = testFunction(1,x_old)
+    denominator = maximize_f2ndDerivative(x_old,epsilon1,r,L)
+    x = x_old - numerator/denominator		#numerator/denominator
+    i = i + 1
+    #print "pl.np.abs(x-x_old) : %.3f" %pl.np.abs(x-x_old)
+    #print "i	: %d" %i
+    #print "x_old: %.3f" %x_old
+    #print "x    : %.3f" %x
+    #print "f(x) : %.3f" %testFunction(0,x_old)
   
+  solution[0] = x
+  solution[1] = testFunction(0,x)
+  solution[2] = i
+  solution_found = True 
+  return solution  
   
   
   return solution
@@ -145,13 +189,48 @@ def goldenSection(a,b,L,r):
       L = b - a
       lambda1 = a + pl.np.power(r,2)*L
     
-    if L < epsilon1:
+    if L < epsilon1 or (i == 10 and args.choice == 3):
       solution[0] = (b+a)/2
       solution[1] = testFunction(order,solution[0])
       solution[2] = i
       solution_found = True
       return solution
 ##########################################################################################
+
+###ADDITIONAL HELPER FUNCTION
+def maximize_f2ndDerivative(x_old,epsilon,r,L):
+  #use goldenSection method to determine maximum point on 2nd derivative function within range
+  #goldenSection is selected instead of Newton-Raphsody method because we assume that we only have
+  #a twice differentiable (smooth) f(x) available. Thus we cant apply Newton-Raphsody to a function
+  #which is already the 2nd derivative.
+  a = x_old - epsilon
+  b = x_old + epsilon
+  L = b - a
+  solution = pl.np.zeros((3,1))
+  i = 0
+  lambda1 = a + pl.np.power(r,2)*L
+  lambda2 = a + r*L
+  solution_found = False
+  order = 2			#this is changed to 2 because we are already working with the 2nd order function
+  
+  while(solution_found == False):
+    f1 = testFunction(order,lambda1)
+    f2 = testFunction(order,lambda2)
+    i = i + 1
+    
+    if(f1 > f2):
+      a = lambda1
+      lambda1 = lambda2
+      L = b - a
+      lambda2 = a + r*L
+    elif (f1 < f2):
+      b = lambda2
+      lambda2 = lambda1
+      L = b - a
+      lambda1 = a + pl.np.power(r,2)*L
+    
+    if L < epsilon or (i == 10 and args.choice == 3):
+      return testFunction(order,(b+a)/2)
 
 
 ###SYMBOLIC TEST_FUNCTIONS f(x), f'(x), f''(x)
@@ -165,7 +244,7 @@ def test1(order,lambdax):
   if order == 0:
     return f.subs(x,lambdax)
   else:
-    f = f.diff(f,order)
+    f = f.diff(x,order)
     return f.subs(x,lambdax)
     
 def test2(order,lambdax):
@@ -177,7 +256,7 @@ def test2(order,lambdax):
   if order == 0:
     return f.subs(x,lambdax)
   else:
-    f = f.diff(f,order)
+    f = f.diff(x,order)
     return f.subs(x,lambdax)
 
 def test3(order,lambdax):
@@ -189,7 +268,7 @@ def test3(order,lambdax):
   if order == 0:
     return f.subs(x,lambdax)
   else:
-    f = f.diff(f,order)
+    f = f.diff(x,order)
     return f.subs(x,lambdax)
   
 def test4(order,lambdax):
@@ -201,7 +280,7 @@ def test4(order,lambdax):
   if order == 0:
     return f.subs(x,lambdax)
   else:
-    f = f.diff(f,order)
+    f = f.diff(x,order)
     return f.subs(x,lambdax) 
 
 '''
@@ -235,7 +314,7 @@ def testFunction(order,lambdax):
 ###RUN SELECTED ALGORITHM
 ##########################################################################################  
 if args.algorithm == 1:
-  sol = lineSearchNewtonRapshon(a,b,L,r)
+  sol = lineSearchNewtonRapshon(a,b)
   algorithm_string = "1-D line search based on the Newton-Raphson Method"
 elif args.algorithm == 2:
   sol = modifiedNewtonRapshon(a,b,L,r)
@@ -265,20 +344,29 @@ j = a
 k = 0
 while k < 50:
   if args.choice == 2:
-    graph.append(-testFunction(order,j))
+    graph.append(-testFunction(0,j))
   else:
-    graph.append(testFunction(order,j))
+    graph.append(testFunction(0,j))
+    
+  #graph1.append(testFunction(1,j))
+  #graph2.append(testFunction(2,j))  
     
   j = j + L/50.0
   k = k + 1
   
 
 pl.plot(pl.np.arange(a,b,L/50.0),graph)
+#pl.plot(pl.np.arange(a,b,L/50.0),graph1)
+#pl.plot(pl.np.arange(a,b,L/50.0),graph2)
 if args.choice == 2:
-  pl.figtext(0.50,0.25, "Total iterations: %d\nLambda solution: %d\nf(lambda solution): %d\n" %(sol[2],sol[0],sol[1]),fontsize=10, bbox=dict(facecolor = 'white', alpha=1, linewidth = 1, edgecolor = 'black'), horizontalalignment = 'center',verticalalignment='top')
+  pl.figtext(0.50,0.35, "Total iterations: %d\nLambda solution: %.3f\nf(lambda solution): %.3f\na: %.2f\nb: %.2f\nepsilon: %.6f\nNewton-Raphson initial lambda value: %.3f" %(sol[2],sol[0],sol[1],a,b,epsilon1,x_initial),fontsize=10, bbox=dict(facecolor = 'white', alpha=1, linewidth = 1, edgecolor = 'black'), horizontalalignment = 'center',verticalalignment='top')
   pl.plot(sol[0], -sol[1], 'g.', markersize=20.0)			#adds a dot where the solution was found
 else:
-  pl.figtext(0.50,0.882, "Total iterations: %d\nLambda solution: %d\nf(lambda solution): %d\n" %(sol[2],sol[0],sol[1]),fontsize=10, bbox=dict(facecolor = 'white', alpha=1, linewidth = 1, edgecolor = 'black'), horizontalalignment = 'center',verticalalignment='top')
+  pl.figtext(0.50,0.882, "Total iterations: %d\nLambda solution: %.3f\nf(lambda solution): %.3f\na: %.2f\nb: %.2f\nepsilon: %.6f\nNewton-Raphson initial lambda value: %.3f" %(sol[2],sol[0],sol[1],a,b,epsilon1,x_initial),fontsize=10, bbox=dict(facecolor = 'white', alpha=1, linewidth = 1, edgecolor = 'black'), horizontalalignment = 'center',verticalalignment='top')
   pl.plot(sol[0], sol[1], 'g.', markersize=20.0)			#adds a dot where the solution was found
+
+
+F = pl.gcf()
+#F.savefig("./graphs/algorithm2/testFunction4.pdf",bbox_inches='tight',format='PDF',pad_inches=0.1)
   
 pl.show()
