b847b21c22b1d9a7594fae2db1c7e6a203847571
Andreas Joss <16450612@sun.ac.za>
2015-03-10 15:38:46 +0200

    Golden section rule is working and graphs are also perfect

--- 1Dline_search.py
+++ 1Dline_search.py
@@ -5,8 +5,211 @@ from __future__ import division				#ensures division always returns the correct 
 import argparse
 import pylab as pl
 import os
+
+###GLOBAL VARIABLES
 ##########################################################################################
-total_runs = 0
-pop = 0                  		#population size
-n_dim = 0                 		#number of dimensions
+#total_runs = 0
+#pop = 0                  		#population size
+#n_dim = 0                 		#number of dimensions
+
+a = -1			#line search start position
+b = 1			#line search end position
+epsilon1 = 0.01		#prescribed accuracy
+#i = 0			#iteration number
+L = b - a		#Length of line search is conducted on
+r = 0.618034		#golden ratio
+algorithm_string = ""
+example_string = ""
+graph = []
 ##########################################################################################
+
+
+###PARSE INFORMATION
+##########################################################################################
+parser = argparse.ArgumentParser(description="Line Search Algorithms",formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument("algorithm", help="Select algorithm type:\n"+
+		    "1 - 1-D line search based on the Newton-Raphson Method\n"+
+		    "2 - Modification of the Newton-Raphson we discussed in class\n"+
+		    "3 - Golden Section Method\n\n", type=int, choices=[1,2,3])					#add a new POSITIONAL argument with a help message included
+
+parser.add_argument("choice", help="Select example function to test the Line Search Algorithms on:\n"+
+		    "1 - Test 1\n"+
+		    "2 - Test 2\n"+
+		    "3 - Test 3\n"+
+		    "4 - Test 4\n\n", type=int, choices=[1,2,3,4])					#add a new POSITIONAL argument with a help message included
+
+#parser.add_argument("i", help="Enter total number of iterations the algorithm should complete", type=int)	#add a new POSITIONAL argument with a help message included
+#parser.add_argument("p", help="Enter desired population the algorithm should use", type=int)			#add a new POSITIONAL argument with a help message included
+#parser.add_argument("n", help="Enter number of dimensions the example problem should have", type=int)		#add a new POSITIONAL argument with a help message included
+#parser.add.argument("filename", help="Enter the name of the file the output figure should be saved to", type=argparse.FileType('w'))
+
+#parser.add_argument("-a","--average", default=1, help="Enter total number of independant runs to calculate average solution", type=int)	#add a new OPTIONAL argument with a help message included
+
+args = parser.parse_args()													#store the argument parsed into the variable "args"
+
+#if args.i != None:
+  #total_runs = args.i
+  
+#if args.p != None:
+  #pop = args.p
+  
+#if args.n != None:
+  #n_dim = args.n
+##########################################################################################
+
+
+###OBTAIN TEST_PARAMETERS
+##########################################################################################
+
+#1 Test function 1
+if args.choice == 1:
+  a = 0
+  b = 2
+  epsilon1 = 0.01
+  example_string = "Test function 1"
+
+#2 Test function 2
+elif args.choice == 2:
+  a = 0
+  b = (pl.np.pi)/2
+  epsilon1 = 0.001
+  example_string = "Test function 2"
+
+#3 Test function 3
+elif args.choice == 3:
+  a = -1.9
+  b = 0.9
+  function_evaluations = 10
+  example_string = "Test function 3"
+
+#4 Test function 4
+elif args.choice == 4:
+  a = 0
+  b = 20
+  epsilon1 = 1e-5
+  example_string = "Test function 4"
+  
+  
+L = b - a		#Length of line search is conducted on
+##########################################################################################
+
+
+###OPTIMIZATION METHODS
+##########################################################################################
+### 1-D line search based on the Newton-Raphson Method
+def lineSearchNewtonRapshon(a,b,L,r):
+  solution = pl.np.zeros((3,1))
+  solution_found = False
+  return solution
+
+### Modification of the Newton-Raphson we discussed in class
+def modifiedNewtonRapshon(a,b,L,r):
+  solution = pl.np.zeros((3,1))
+  solution_found = False
+  return solution
+
+### Golden Section Method
+def goldenSection(a,b,L,r):
+  solution = pl.np.zeros((3,1))
+  i = 0
+  lambda1 = a + pl.np.power(r,2)*L
+  lambda2 = a + r*L
+  solution_found = False
+  
+  while(solution_found == False):
+    f1 = testFunction(lambda1)
+    f2 = testFunction(lambda2)
+    i = i + 1
+    
+    if(f1 > f2):
+      a = lambda1
+      lambda1 = lambda2
+      L = b - a
+      lambda2 = a + r*L
+    elif (f1 < f2):
+      b = lambda2
+      lambda2 = lambda1
+      L = b - a
+      lambda1 = a + pl.np.power(r,2)*L
+    
+    if L < epsilon1:
+      solution[0] = (b+a)/2
+      solution[1] = testFunction(solution[0])
+      solution[2] = i
+      solution_found = True
+      return solution
+##########################################################################################
+
+
+###TEST_FUNCTIONS
+##########################################################################################
+def test1(lambdax):
+  return pl.np.power(lambdax,2) + 2*pl.np.exp(-lambdax)
+
+def test2(lambdax):
+  return -lambdax*pl.np.cos(lambdax)	#maximize
+
+def test3(lambdax):
+  return 4*(lambdax-7)/(pl.np.power(lambdax,2) + lambdax - 2)
+
+def test4(lambdax):
+  return pl.np.power(lambdax,4) - 20*pl.np.power(lambdax,3) + 0.1*lambdax
+
+def testFunction(lambdax):
+  if args.choice == 1:
+    return test1(lambdax)
+  elif args.choice == 2:
+    return test2(lambdax)	
+  elif args.choice == 3:
+    return test3(lambdax)
+  elif args.choice == 4:
+    return test4(lambdax)
+##########################################################################################  
+
+###RUN SELECTED ALGORITHM
+##########################################################################################  
+if args.algorithm == 1:
+  sol = lineSearchNewtonRapshon(a,b,L,r)
+  algorithm_string = "1-D line search based on the Newton-Raphson Method"
+elif args.algorithm == 2:
+  sol = modifiedNewtonRapshon(a,b,L,r)
+  algorithm_string = "Modification of the Newton-Raphson we discussed in class"
+elif args.algorithm == 3:
+  sol = goldenSection(a,b,L,r)
+  algorithm_string = "Golden Section Method"
+
+print "The following algorithm was used : " + algorithm_string
+print "The following test function was tested : " + example_string
+print "Total number of iterations:	%d" %sol[2]
+print "lambda solution = %.3f" %sol[0]
+print "f solution = %.3f" %sol[1]
+
+fig1 = pl.figure(1)
+pl.title(algorithm_string + " evaluated by " + example_string)
+pl.xlabel("lambda")
+pl.ylabel("f(lambda)")
+
+#is hierdie a en b weer global waardes?
+print "a = %.2f" %a
+print "b = %.2f" %b
+
+j = a
+k = 0
+while k < 50:
+  if args.choice == 2:
+    graph.append(-testFunction(j))
+  else:
+    graph.append(testFunction(j))
+    
+  j = j + L/50.0
+  k = k + 1
+  
+
+pl.plot(pl.np.arange(a,b,L/50.0),graph)
+if args.choice == 2:
+  pl.plot(sol[0], -sol[1], 'g.', markersize=20.0)			#adds a dot where the solution was found
+else:
+  pl.plot(sol[0], sol[1], 'g.', markersize=20.0)			#adds a dot where the solution was found
+  
+pl.show()
